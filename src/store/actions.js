60a4aa946162b25c5c4a1d77e72302da77017cbe
ShojiroAbe <astook89@gmail.com>
2022-06-29 00:12:14 +0900

    アクションの雛形

---
+++ src/store/actions.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import * as types from './mutation-types'
+import { Auth, Listm, Task } from '../api'
+/* eslint-disable no-unused-vars */
+
+export default {
+  login: ({ commit }) => {
+    // TODO:
+    throw new Error('login実装してください')
+  },
+  
+  fetchLists: ({ commit }) => {
+    // TODO:
+    throw new Error('fetchLists実装してください')
+  },
+  
+  addTask: ({ commit }) => {
+    // TODO:
+    throw new Error('addTask実装してください')
+  },
+
+  updateTask: ({ commit }) => {
+    // TODO:
+    throw new Error('updateTask実装してください')
+  },
+
+  removeTask: ({ commit }) => {
+    // TODO:
+    throw new Error('removeTask実装してください')
+  },
+
+  logout: ({ commit }) => {
+    // TODO:
+    throw new Error('logout実装してください')
+  }
+}
