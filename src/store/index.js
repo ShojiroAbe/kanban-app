5eeaac007b5955121dfaae97f97b4b4688312124
ShojiroAbe <astook89@gmail.com>
2022-06-28 23:42:19 +0900

    状態のモデリング

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,19 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 状態「Auth」と状態「Bord」をVuexのstateで一元管理できるよう定義する
+const state = {
+  auth: {
+    token: null, // tokenはnullで初期化
+    userId: null // userIdはnullで初期化
+  },
+  bord: {
+    lists: [] // 状態TaskListは空で初期化
+  }
+}
+
 export default new Vuex.Store({
+  state, // 定義したstateを「state」オプションに指定
   getters,
   actions,
   mutations,
