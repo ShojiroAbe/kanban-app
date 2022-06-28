a6b684c6b42a0fa763318cbce00bd28892e3990c
ShojiroAbe <astook89@gmail.com>
2022-06-29 00:01:22 +0900

    データフロー周りの雛形コードのセットアップ

---
+++ src/store/mutations.js
@@ -0,0 +1,28 @@
+import * as types from './mutation-types'
+
+export default {
+  [types.AUTH_LOGIN] (state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGINを実装してください')
+  },
+  [types.FETCH_ALL_TASKLIST] (state, payload) {
+    // TODO:
+    throw new Error('FETCH_ALL_TASKLISTを実装してください')
+  },
+  [types.ADD_TASK] (state, payload) {
+    // TODO:
+    throw new Error('ADD_TASKを実装してください')
+  },
+  [types.UPDATE_TASK] (state, payload) {
+    // TODO:
+    throw new Error('UPDATE_TASKを実装してください')
+  },
+  [types.REMOVE_TASK] (state, payload) {
+    // TODO:
+    throw new Error('REMOVE_TASKを実装してください')
+  },
+  [types.AUTH_LOGOUT] (state, payload) {
+    // TODO:
+    throw new Error('AUTH_LOGOUTを実装してください')
+  }
+}
